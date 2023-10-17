d9fbef4ecfe0642844e69325051cc9c6b3b6f4c5
никитка <nikita47owchinnikow@gmail.com>
2023-10-17 14:14:18 +0700

    Добавление функционала для сохранения записи после выхода из приложения

--- app/src/main/java/com/example/pr16ovchinnikov/MainActivity.java
+++ app/src/main/java/com/example/pr16ovchinnikov/MainActivity.java
@@ -1,57 +1,64 @@
 package com.example.pr16ovchinnikov;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private EditText inputEditText;
-    private Button saveButton;
-    private Button showButton;
+    private Button saveButton,showButton;
     private TextView outputTextView;
-    private String savedValue;
+    private String savedLine = "undefined";
+    private String namePreferences = "Settings";
+    final static String KeyLine = "ID_Line";
 
+    SharedPreferences settings;//настройки
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         inputEditText = findViewById(R.id.inputText);
         saveButton = findViewById(R.id.save);
         showButton = findViewById(R.id.move);
         outputTextView = findViewById(R.id.moveText);
-
-        saveButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                savedValue = inputEditText.getText().toString();
-                Toast.makeText(MainActivity.this, "строка: <" + savedValue + "> сохранена" , Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        showButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                outputTextView.setText(savedValue);
-            }
-        });
+        settings = getSharedPreferences(namePreferences, MODE_PRIVATE);
+        saveButton.setOnClickListener(this);
+        showButton.setOnClickListener(this);
     }
 
     @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-        savedValue = savedInstanceState.getString("savedValue");
+    public void onClick(View view) {
+        if(view.getId() == R.id.save){
+            savedLine = inputEditText.getText().toString();
+            SharedPreferences.Editor prefEditor = settings.edit();//строка для возможности вносить изменения
+            prefEditor.putString(KeyLine, savedLine);//внесение изменений
+            prefEditor.apply();//сохранение внесенных изменений
+        }
+        else if (view.getId() == R.id.move){
+            savedLine = settings.getString(KeyLine, "Строка ещё не была сохранена");
+            outputTextView.setText(savedLine);
+        }
     }
-
+    //метод для сохранения строки
     @Override
-    protected void onSaveInstanceState(Bundle outState) {
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.putString(KeyLine, savedLine);
         super.onSaveInstanceState(outState);
-        outState.putString("savedValue", savedValue);
+    }
+    //метод для получения сохраненной строки по ключу
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        savedLine = savedInstanceState.getString(KeyLine);
+        outputTextView.setText(savedLine);
     }
 }
 
